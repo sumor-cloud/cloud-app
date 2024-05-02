a02f8ba582d4b1160bd6b258c886aed936c0c9ce
Dennis Chen <70829793@qq.com>
2024-05-02 16:34:40 +0800

    remove pack module

--- src/entry/build.js
+++ src/entry/build.js
@@ -1,27 +1,7 @@
-import pack from '../pack/index.js'
 import app from '../app/index.js'
 
 export default async (options) => {
-  const debug = !!options.debug
-  const type = options.type || 'app'
-  const mode = debug ? 'development' : 'production'
-
-  switch (type) {
-    case 'vue':
-      await pack({
-        mode
-      })
-      break
-    case 'node':
-      await pack({
-        mode,
-        node: true
-      })
-      break
-    default:
-      await app({
-        mode: 'build'
-      })
-      break
-  }
+  await app({
+    mode: 'build'
+  })
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 // entry
 import app from './app/index.js'
 import cli from './entry/cli.js'
-import pack from './pack/index.js'
 
 export default {
   app,
-  pack,
   cli
 }
