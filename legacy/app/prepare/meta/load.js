c182093e550aa071319093cf6a64f8e52920ebd8
Dennis Chen <70829793@qq.com>
2024-05-22 15:02:07 +0800

    refactor

--- legacy/app/prepare/meta/load.js
+++ legacy/app/prepare/meta/load.js
@@ -1,25 +1,12 @@
 import fse from 'fs-extra'
 import { pathToFileURL } from 'url'
 
-import sumorText from './sumorObjects/text.js'
-import sumorType from './sumorObjects/type.js'
 import findFiles from '../../../utils/findFiles.js'
 import parseFileName from '../../../utils/parseFileName.js'
 import loadMeta from '../../../../src/context/loadMeta.js'
 
 export default async context => {
-  const meta = {
-    text: sumorText,
-    type: sumorType
-  }
-
-  const jsonMeta = await loadMeta(context.root)
-  for (const i in jsonMeta) {
-    meta[i] = jsonMeta[i]
-  }
-
-  await fse.ensureDir(`${process.cwd()}/tmp`)
-  await fse.writeFile(`${process.cwd()}/tmp/meta.json`, JSON.stringify(meta, null, 4))
+  const meta = await loadMeta(context.root)
 
   // 获取event程序对象文件
   const eventRootPath = `${context.root}/event`
--- legacy/app/serve/handler/checkData.js
+++ legacy/app/serve/handler/checkData.js
@@ -7,8 +7,8 @@ export default (data, meta) => {
     data[key] = format(meta.parameters[key], data[key])
     const fieldErrors = validate(
       {
-        error: true,
-        ...meta.parameters[key]
+        ...meta.parameters[key],
+        error: true
       },
       data[key]
     )
@@ -17,75 +17,5 @@ export default (data, meta) => {
   if (errors.length > 0) {
     throw new CloudAppError('SUMOR_API_FIELDS_VALIDATION_FAILED', {}, errors)
   }
-  //   const definition = meta.parameters[key]
-  //
-  //
-  //   // 补充默认值
-  //   if (definition.default !== undefined) {
-  //     if (definition.type === 'string') {
-  //       if (data[key] === null || data[key] === undefined || data[key] === '') {
-  //         data[key] = definition.default
-  //       }
-  //     } else if (data[key] === null || data[key] === undefined) {
-  //       data[key] = definition.default
-  //     }
-  //   }
-  //
-  //   // 强制类型转换
-  //   switch (definition.type) {
-  //     case 'string':
-  //       if (data[key] !== null && data[key] !== undefined && typeof data[key] !== 'string') {
-  //         data[key] = data[key].toString()
-  //       }
-  //       break
-  //     case 'number':
-  //       if (typeof data[key] !== 'number' && data[key] !== null && data[key] !== undefined) {
-  //         if (typeof data[key] === 'string') {
-  //           data[key] = parseFloat(data[key])
-  //           if (isNaN(data[key])) {
-  //             data[key] = null
-  //           }
-  //         } else {
-  //           delete data[key]
-  //         }
-  //       }
-  //       break
-  //     default:
-  //       break
-  //   }
-  //
-  //   // 强制修正数据
-  //   if (data[key]) {
-  //     if (definition.trim) {
-  //       data[key] = data[key].trim()
-  //     }
-  //     if (definition.upperCase) {
-  //       data[key] = data[key].toUpperCase()
-  //     }
-  //     if (definition.lowerCase) {
-  //       data[key] = data[key].toLowerCase()
-  //     }
-  //   }
-  //
-  //   // 校验数据
-  //   if (
-  //     definition.required === true &&
-  //     (data[key] === undefined || data[key] === null || data[key] === '')
-  //   ) {
-  //     throw new Error('sumorApp.REQUIRED')
-  //   }
-  //   if (definition.length && data[key] && data[key].length > definition.length) {
-  //     const error = new Error('sumorApp.LENGTH_OUT_OF_LIMIT')
-  //     error.data = { length: definition.length }
-  //     throw error
-  //   }
-  //   console.log("definition",definition)
-  //   for (const rule of definition.rule) {
-  //     const matched = new RegExp(rule.expression).test(data[key])
-  //     if (!matched) {
-  //       throw new Error(rule.text)
-  //     }
-  //   }
-  // }
   return data
 }
