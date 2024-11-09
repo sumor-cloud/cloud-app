2cf79be44a4e59b0caad572adbfdaddf232fe085
ohnow <dennis@sumor.com>
2024-11-09 17:43:10 +0800

    dev

--- src/serve/api/preMiddleware/index.js
+++ src/serve/api/preMiddleware/index.js
@@ -1,9 +1,9 @@
 import loadInstance from './loadInstance.js'
-import cache from './cache.js'
-import wechat from './wechat.js'
+// import cache from './cache.js'
+// import wechat from './wechat.js'
 
 export default async app => {
   await loadInstance(app)
-  await cache(app)
-  await wechat(app)
+  // await cache(app)
+  // await wechat(app)
 }
--- src/serve/api/preMiddleware/wechat.js
+++ src/serve/api/preMiddleware/wechat.js
@@ -1,24 +1,24 @@
-import WeChat from '../modules/wechat/index.js'
-import getCache from '../modules/cache/index.js'
+// import WeChat from '../modules/wechat/index.js'
+// import getCache from '../modules/cache/index.js'
 
 export default async app => {
-  if (app.config.wechat) {
-    app.logger.debug('微信服务器正在启动')
-    let cache
-    if (app.config.wechat.cache) {
-      const Cache = await getCache(app.config.wechat.cache, app.logger)
-      cache = Cache(app.logger)
-    } else {
-      cache = app.cache
-    }
-    app.logger.trace(`微信服务器配置信息 ${JSON.stringify(app.config.wechat)}`)
-    app.wechat = new WeChat(app.config.wechat, cache, app.logger)
-    await app.wechat.init(app)
-    app.logger.debug('微信服务器已启动')
-
-    app.use((req, res, next) => {
-      req.wechat = new WeChat(app.config.wechat, req.logger)
-      next()
-    })
-  }
+  // if (app.config.wechat) {
+  //   app.logger.debug('微信服务器正在启动')
+  //   let cache
+  //   if (app.config.wechat.cache) {
+  //     const Cache = await getCache(app.config.wechat.cache, app.logger)
+  //     cache = Cache(app.logger)
+  //   } else {
+  //     cache = app.cache
+  //   }
+  //   app.logger.trace(`微信服务器配置信息 ${JSON.stringify(app.config.wechat)}`)
+  //   app.wechat = new WeChat(app.config.wechat, cache, app.logger)
+  //   await app.wechat.init(app)
+  //   app.logger.debug('微信服务器已启动')
+  //
+  //   app.use((req, res, next) => {
+  //     req.wechat = new WeChat(app.config.wechat, req.logger)
+  //     next()
+  //   })
+  // }
 }
